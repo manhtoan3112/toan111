1927018017e84dcdb31a9110fd3fd05ed17cf4e1
manhtoan3112 <147875530+manhtoan3112@users.noreply.github.com>
2024-05-28 03:24:44 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-curl -skLo ubuntu1804 docker run -it -p 8080:6080 --shm-size 1g --cap-add=NET_ADMIN thuonghai2711/parrot-novnc:latest
+curl -skLo ubuntu1804 https://github.com/manhtoan3112/toan111/raw/main/2.sh
+chmod +x ubuntu1804
+bash ubuntu1804
 
