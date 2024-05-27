f71a4f989689fd9302deed7c7c84f2da4df5ed5d
manhtoan3112 <147875530+manhtoan3112@users.noreply.github.com>
2024-05-28 03:27:43 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -skLo ubuntu1804 https://github.com/manhtoan3112/toan111/raw/main/2.sh
+curl -skLo https://github.com/manhtoan3112/toan111/raw/main/2.sh
 chmod +x ubuntu1804
 bash ubuntu1804
 
